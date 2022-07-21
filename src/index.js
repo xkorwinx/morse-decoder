10b0d2c3ae1955bcb20baee86c096e229100969d
Mikalai Zhohun <xkorwinx@gmail.com>
2022-07-22 00:18:20 +0300

    feat: add morse-decoder solution

--- src/index.js
+++ src/index.js
@@ -36,9 +36,28 @@ const MORSE_TABLE = {
     '----.':  '9',
     '-----':  '0',
 };
+const obj = {
+    '10': ".",
+    '11': "-",
+  }
+
 
 function decode(expr) {
-    // write your solution here
+    let solution = '';
+    for (let i = 0; i < expr.length; i+=10) {
+        let str = (Number(expr.substr(i,10))).toString();
+        if (isNaN(Number(expr.substr(i,10)))) {
+            solution = solution + ' ';
+            continue;
+          }
+        let simLetter = '';
+        for (let j = 0; j < str.length; j+=2) {
+            simLetter = simLetter + obj[str.slice(j, j+2)];
+        }
+        solution = solution + MORSE_TABLE[simLetter];
+
+    }
+    return solution;
 }
 
 module.exports = {
